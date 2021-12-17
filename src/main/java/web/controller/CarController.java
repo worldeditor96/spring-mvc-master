7124eda45dedf32667a7bf7f5206ae9862c580e5
Maksim Penza <w_max_hot_e@mail.ru>
2021-12-17 23:28:27 +0300

    17.12.2021

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.Service.CarServiceImpl;
 import web.models.Car;
 
@@ -16,6 +17,30 @@ import java.util.List;
 @Controller
 public class CarController {
 
+    @GetMapping("/cars")
+    public String carView(@RequestParam(value = "count", required = false) String count, Model model) {
+        Car car1 = new Car("tractor", "dirty", 5);
+        Car car2 = new Car("Lada", "red", 110);
+        Car car3 = new Car("Ford", "black", 150);
+        Car car4 = new Car("VW", "broun", 180);
+        Car car5 = new Car("Audi", "black", 250);
+        List<Car> list = new ArrayList<Car>();
+        list.add(car1);
+        list.add(car2);
+        list.add(car3);
+        list.add(car4);
+        list.add(car5);
+        model.addAttribute("cars", list);
+
+        CarServiceImpl carService=new CarServiceImpl();
+
+        carService.getTable(list, count);
+
+        return "cars";
+    }
+
+        /*
+    //Способо №2 через HttpServletRequest request
     @GetMapping("/cars")
     public String carView(Model model, HttpServletRequest request) {
         Car car1 = new Car("tractor", "dirty", 5);
@@ -35,11 +60,12 @@ public class CarController {
 
         CarServiceImpl carService=new CarServiceImpl();
 
-
         carService.getTable(list, count);
 
         return "cars";
     }
+     */
+
 }
 
 
